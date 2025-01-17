0a46aa48bfe8fedc66e68d9e6064666dcfa131b3
kai <help.rajsarkar@gmail.com>
2025-01-17 23:43:31 +0530

    Project complete

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from data import *
 
 THEME_COLOR = "#375362"
 
@@ -17,11 +18,11 @@ class QuizInterface():
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_btn = Button(image=true_image, highlightthickness=0)
+        self.true_btn = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
         self.true_btn.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_btn = Button(image=false_image, highlightthickness=0)
+        self.false_btn = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
         self.false_btn.grid(row=2, column=1)
 
         self.get_next_question()
@@ -30,5 +31,27 @@ class QuizInterface():
 
 
     def get_next_question(self):
-         q_text = self.quiz.next_question()
-         self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="The Quiz has finished")
+            self.true_btn.config(state=DISABLED)
+            self.false_btn.config(state=DISABLED)
+
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+    def false_pressed(self):
+        is_wrong = self.quiz.check_answer("False")
+        self.give_feedback(is_wrong)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
